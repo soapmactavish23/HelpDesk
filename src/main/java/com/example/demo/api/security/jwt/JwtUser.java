d474ea8ff5e497cef9c9b200141c38eff1f342f5
HKprogram <ricknogueira1231@gmail.com>
2021-10-25 18:12:09 -0300

    Autenticação e autorização com tokens JWT Parte 1

---
+++ src/main/java/com/example/demo/api/security/jwt/JwtUser.java
@@ -0,0 +1,67 @@
+package com.example.demo.api.security.jwt;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtUser implements UserDetails {
+	
+	private static final long serialVersionUID = -7471177486146141709L;
+	
+	private final String id;
+	private final String username;
+	private final String password;
+	private final Collection<? extends GrantedAuthority> authorities;
+
+	public JwtUser(String id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.authorities = authorities;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+
+}
