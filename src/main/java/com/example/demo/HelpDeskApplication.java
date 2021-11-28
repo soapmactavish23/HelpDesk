51b0ca4a2004729dc69242d381f60239194177b4
soapmactavish23 <ricknogueira1231@gmail.com>
2021-11-28 00:24:38 -0300

    TOKEN COM ERRO

--- src/main/java/com/example/demo/HelpDeskApplication.java
+++ src/main/java/com/example/demo/HelpDeskApplication.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.api.repository.UserRepository;
 
 @SpringBootApplication
 public class HelpDeskApplication {
 
+	@Autowired
+	UserRepository userRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskApplication.class, args);
 	}
--- src/test/java/com/example/demo/HelpDeskApplicationTests.java
+++ src/test/java/com/example/demo/HelpDeskApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.api.entity.User;
+import com.example.demo.api.enums.ProfileEnum;
+import com.example.demo.api.repository.UserRepository;
 
 @SpringBootTest
 class HelpDeskApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Test
 	void contextLoads() {
+		User user = new User();
+		user.setEmail("admin@admin.com");
+		user.setPassword(passwordEncoder.encode("123456"));
+		user.setProfile(ProfileEnum.ROLE_ADMIN);
+		userRepository.save(user);
 	}
 
 }
