d1d8d1b257a05ca044a041b09502509110236841
soapmactavish23 <ricknogueira1231@gmail.com>
2021-11-28 20:32:18 -0300

    Criando o Controller de Usuário Parte 2

--- src/main/java/com/example/demo/api/controller/UserController.java
+++ src/main/java/com/example/demo/api/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.example.demo.api.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +43,6 @@ public class UserController {
 	public ResponseEntity<Response<User>> create(HttpServletResponse request, @RequestBody User user, BindingResult result) {
 		Response<User> response = new Response<User>();
 		try {
-			
 			validateCreatedUser(user, result);
 			if(result.hasErrors()) {
 				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
@@ -51,7 +58,7 @@ public class UserController {
 			response.getErrors().add(e.getMessage());
 			return ResponseEntity.badRequest().body(response);
 		}
-		return ResponseEntity.ok(response);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 	
 	private void validateCreatedUser(User user, BindingResult result) {
@@ -59,5 +66,73 @@ public class UserController {
 			result.addError(new ObjectError("User", "Email no information"));
 		}
 	}
+	
+	
+	@PutMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> update(HttpServletResponse request, @RequestBody User user, BindingResult result) {
+		Response<User> response = new Response<User>();
+		try {
+			validateUpdateUser(user, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			User userPersisted = (User) userService.createOrUpdate(user);
+			response.setData(userPersisted);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	private void validateUpdateUser(User user, BindingResult result) {
+		if(user.getId() == null) {
+			result.addError(new ObjectError("User", "Id no information"));
+		}
+		if(user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email no information"));
+		}
+	}
+	
+	@GetMapping(value = "{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> findById(@PathVariable("id") String id) {
+		Response<User> response = new Response<User>();
+		Optional<User> userOptional = userService.findById(id);
+		User user = userOptional.get();
+		if (user == null) {
+			response.getErrors().add("Register not found id:" + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+		response.setData(user);
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") String id) {
+		Response<String> response = new Response<String>();
+		Optional<User> userOptional = userService.findById(id);
+		User user = userOptional.get();
+		if (user == null) {
+			response.getErrors().add("Register not found id:" + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+		userService.delete(id);
+		return ResponseEntity.ok(new Response<String>());
+	}
+	
+	
+	@GetMapping(value = "{page}/{count}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+    public  ResponseEntity<Response<Page<User>>> findAll(@PathVariable int page, @PathVariable int count) {
+		Response<Page<User>> response = new Response<Page<User>>();
+		Page<User> users = userService.findAll(page, count);
+		response.setData(users);
+		return ResponseEntity.ok(response);
+    }
 	
 }
